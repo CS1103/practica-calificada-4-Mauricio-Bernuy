20c62e756ecbb5a09bcf514376e94c73102f5e41
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2019-12-02 10:19:54 -0500

    small fix

--- main.cpp
+++ main.cpp
@@ -14,13 +14,14 @@ int main()
     unsigned int w;
     unsigned int h;
     auto image = decode("../in.png", w, h);
-	thread t1(rotate, ref(image), w, h);
+	auto img2 = image;
+	thread t1(rotate, ref(img2), w, h);
 
 	encode("../red_filter.png", keep('R', image, w, h), w, h);
 	encode("../green_filter.png", keep('G', image, w, h) , w, h);
 	encode("../blue_filter.png", keep('B', image, w, h), w, h);
 	
 	t1.join();
-	encode("../rotated.png", image, w, h);
+	encode("../rotated.png", img2, w, h);
     return 0;
 }
